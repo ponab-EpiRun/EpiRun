1938825a8d48cf9153eaa3850dfdfe2f03c042ae
ponab-EpiRun <ponab@uoc.edu>
2026-04-09 23:42:17 +0200

    Ajustado salto y creado salto doble

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,31 +2,42 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-        //Ajustar fuerza de salto
+    // Ajustar fuerza de salto
     public float jumpForce = 5f;
+
     private Rigidbody2D rb;
     private bool isGrounded = true;
+    private bool doubleJumpAvailable = false;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-        void Update()
+    void Update()
+    {
+        // Salto normal desde el suelo
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
-            if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-            {
-                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
-                isGrounded = false;
-            }
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            isGrounded = false;
+            doubleJumpAvailable = true;
         }
+        // Doble salto en el aire
+        else if (Input.GetKeyDown(KeyCode.Space) && !isGrounded && doubleJumpAvailable)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            doubleJumpAvailable = false;
+        }
+    }
 
-
-            void OnCollisionEnter2D(Collision2D collision)
-            {               //Suelo = objeto Ground
-                    if (collision.gameObject.name == "Ground")
-                    {
-                        isGrounded = true;
-                    }
-            }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Al tocar el suelo, se reinician los saltos
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+            doubleJumpAvailable = false;
+        }
+    }
 }
